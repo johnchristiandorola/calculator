94a7f74cdeccea8c6041ddd9d51e2ebf9119ffb4
John Christian Dorola <dorolajohnchristian@gmail.com>
2025-03-18 22:14:35 +0800

    added the logics

--- lib/pages/calc_page.dart
+++ lib/pages/calc_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:my_calculator/widget/button_widget.dart';
+import 'package:math_expressions/math_expressions.dart';
 
 class CalcPage extends StatefulWidget {
   const CalcPage({super.key});
@@ -33,27 +34,62 @@ class _CalcPageState extends State<CalcPage> {
     'ANS',
     '=',
   ];
+
+  bool isOperator(String x) {
+    if (x == '%' ||
+        x == '/' ||
+        x == 'x' ||
+        x == '-' ||
+        x == '+' ||
+        x == '=' ||
+        x == 'ANS') {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  void equalPressed() {
+    String question = userInputs;
+    question = question.replaceAll('x', '*');
+
+    ExpressionParser p = GrammarParser();
+    Expression exp = p.parse(question);
+    ContextModel cm = ContextModel();
+    double eval = exp.evaluate(EvaluationType.REAL, cm);
+
+    answer = eval.toString();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
-        SizedBox(height: 20.0),
+        SizedBox(height: 50.0),
         Expanded(
           child: Container(
+            margin: EdgeInsets.symmetric(horizontal: 20),
             child: Column(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
                 Container(
-                  color: Colors.white,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(8),
+                    color: Colors.white,
+                  ),
                   padding: EdgeInsets.all(15),
                   alignment: Alignment.bottomLeft,
-                  child: Text(userInputs, style: TextStyle(fontSize: 20.0)),
+                  child: Text(userInputs, style: TextStyle(fontSize: 25.0)),
                 ),
                 Container(
-                  color: Colors.white,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(8),
+                    color: Colors.white,
+                  ),
+
                   padding: EdgeInsets.all(15),
                   alignment: Alignment.bottomRight,
-                  child: Text(answer, style: TextStyle(fontSize: 20.0)),
+                  child: Text(answer, style: TextStyle(fontSize: 25.0)),
                 ),
               ],
             ),
@@ -70,15 +106,26 @@ class _CalcPageState extends State<CalcPage> {
               itemBuilder: (context, index) {
                 return ButtonWidget(
                   onTapped: () {
+                    String clickedButton = listOfButtons[index];
                     setState(() {
                       if (index == 0) {
                         userInputs = '';
+                        answer = '';
                       } else if (index == 1) {
                         userInputs = userInputs.substring(
                           0,
                           userInputs.length - 1,
                         );
+                      } else if (index == listOfButtons.length - 1 ||
+                          index == listOfButtons.length - 2) {
+                        setState(() {
+                          equalPressed();
+                        });
                       } else {
+                        if (isOperator(listOfButtons[index]) &&
+                            isOperator(userInputs[userInputs.length - 1])) {
+                          return;
+                        }
                         userInputs += listOfButtons[index];
                       }
                     });
@@ -99,19 +146,5 @@ class _CalcPageState extends State<CalcPage> {
         ),
       ],
     );
-  }
-
-  bool isOperator(String x) {
-    if (x == '%' ||
-        x == '/' ||
-        x == 'x' ||
-        x == '-' ||
-        x == '+' ||
-        x == '=' ||
-        x == 'ANS') {
-      return true;
-    } else {
-      return false;
-    }
   }
 }
